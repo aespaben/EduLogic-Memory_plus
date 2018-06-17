5f2f5cc56dd465ffb67b490269cb4b46f83e06df
Angel España <a.espaben@gmail.com>
2018-06-17 19:13:23 -0400

    Create about.h

---
+++ memory/headers/about.h
@@ -0,0 +1,27 @@
+/**
+ * @file about.h
+ * @author Angel España - GitHub aespaben
+ * @author Patricia Daboin - GitHub apdaboin
+ */
+#ifndef ABOUT_H
+#define ABOUT_H
+
+#include <QDialog>
+
+namespace Ui {
+class about;
+}
+
+class about : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit about(QWidget *parent = 0);
+    ~about();
+
+private:
+    Ui::about *ui;
+};
+
+#endif // ABOUT_H
